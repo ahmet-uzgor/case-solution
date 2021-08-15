e3783cc8b2252b5af4abbca9dc7f240de4c9c751
Ahmet Üzgör <ahmet@90pixel.com>
2021-08-15 20:09:32 +0300

    small fix

--- db/db-queries.js
+++ db/db-queries.js
@@ -8,23 +8,24 @@ const getRecordWithFilter = async (startDate, endDate, minCount, maxCount) => {
   try {
     const records = await Record.aggregate([
       {
-        $project: {
+        $project: { // it gets desired parameters
           _id: false,
           key: true,
           createdAt: true,
-          totalCount: { $sum: "$counts" }
+          totalCount: { $sum: "$counts" } // it gives sum of array to be able to filter totalCount
         },          
       },
       { 
         $match: {
           totalCount: { $gte: minCount, $lte: maxCount }, // it filters sum of counts between given min and maxCounts
-          createdAt: { // it filters created_At date between start and endDate in suitable date type 
+          createdAt: { // it filters created_At date between start and endDate in suitable date type (it is converted to utc in validation)
             $gte: startDate, 
             $lte: endDate
           }
         }
       },
     ]);
+    
     return { records, error: null }
   } catch (error) {
     return { records: [], error: error.message }
