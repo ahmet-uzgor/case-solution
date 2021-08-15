d9fe46a0646bf028a31710168a0b6ecdd4dbcf9e
Ahmet Üzgör <ahmet@90pixel.com>
2021-08-15 16:07:00 +0300

    added all validation, error handler file in handler folder

---
+++ handlers/http-status-codes.js
@@ -0,0 +1,9 @@
+// All Necesssary http status codes are listed in a object to use in request handling
+const httpStatusCodes = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER: 500
+}
+   
+module.exports = httpStatusCodes
