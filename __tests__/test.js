45d6e1aa187cf89e82f579af5b0208b988bf4067
Ahmet Üzgör <ahmet@90pixel.com>
2021-08-16 09:02:36 +0300

    added new test cases (14 tests all passed)

--- __tests__/test.js
+++ __tests__/test.js
@@ -20,6 +20,19 @@ describe('routes url path notFound', () => {
     });
 });
 
+describe('"/" api health check', () => {
+  it('tests api health check', (done) => {
+    request
+      .get('/')
+      .expect(httpStatusCodes.OK)
+      .end((err, res) => {
+        if(err) return done(err)
+        expect(res.body.message).toBe('System is working very well')
+        return done()
+      })
+  })
+})
+
 // ROUTE TEST, it tests route endpoints
 describe('/filterRecords test cases', () => {
   let requestData;
@@ -180,17 +193,44 @@ describe('/filterRecords test cases', () => {
 
 // DB_QUERIES FUNCTION TESTS
 describe('query functions test cases', () => {
+  // mock function to test getRecordWithFilter
+  const mockGetRecordWithFilter = jest.fn(); // it creates mock function
+  mockGetRecordWithFilter.mockReturnValueOnce({ records: [
+    {
+      "key":"TAKwGc6Jr4i8Z487",
+      "createdAt":"2017-01-28T01:22:14.398Z",
+      "totalCount":2800
+    }
+  ], error: null }).mockReturnValueOnce({ records: [], error: new Error('DB error => Receive timed out') });
+  // mockReturn simulates return behaviour
   // sample request body
   let startDate = new Date('2016-01-26');
   let endDate = new Date('2018-02-02');
   let minCount = 2700;
   let maxCount = 3000;
 
+  // Test with real function 
   it('getRecordWithFilter function with suitable given parameters', (done) => {
     // all parameters of getRecordWithFilter is checked by validation rules , so no necessary to add a testcase
     const { records, error } = getRecordWithFilter(startDate, endDate, minCount, maxCount);
     expect(error).toBeUndefined()
     return done()
+  });
+
+  it('getRecordWithFilter(mock) function respond with data and null error', (done) => {
+    const result = mockGetRecordWithFilter();
+    expect(result.error).toBeNull();
+    expect(Object.keys(result.records[0])).toContain('key');
+    expect(Object.keys(result.records[0])).toContain('createdAt');
+    expect(Object.keys(result.records[0])).toContain('totalCount');
+    return done();
+  });
+
+  it('getRecordWithFilter(mock) function respond with error and null records array', (done) => {
+    const result = mockGetRecordWithFilter();
+    expect(result.records.length).toBe(0);
+    expect(result.error.message).toBe('DB error => Receive timed out');
+    return done();
   })
 })
 
--- db/db-connection.js
+++ db/db-connection.js
@@ -6,14 +6,4 @@ module.exports = async () => {
         useCreateIndex: true,
         useUnifiedTopology: process.env.TOPOLOGY == 'false' ? false : true // when test ran, it throws error if it is true
     });
-
-    mongoose.connection.on('open', () => {
-        console.log('Connected to DB');
-    });
-
-    mongoose.connection.on('error', (err) => {
-        console.log("DB error: ", err);
-    });
-
-    mongoose.Promise = global.Promise;
 };
